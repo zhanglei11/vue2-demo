195fb98b72039c394f8b746af0e99c92deeb143d
zhanglei5757 <1315500916@qq.com>
2021-04-26 16:28:26 +0800

    no message

---
+++ .history/src/views/About_20210426155801.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <el-button type="primar"></el-button>
+  </div>
+</template>
---
+++ .history/src/views/About_20210426155808.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <el-button type="primary">dao'ch</el-button>
+  </div>
+</template>
